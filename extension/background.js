c7aa48a19e231654b059a402907079f475740dc9
Steven Suh <stevenesuh@gmail.com>
2019-07-18 02:56:12 -0700

    fix

--- extension/background.js
+++ extension/background.js
@@ -1,19 +1,17 @@
 const URL = 'https://us-central1-history-police.cloudfunctions.net';
 
-chrome.runtime.onInstalled.addListener(function () {
-  chrome.history.onVisited.addListener(function (historyItem) {
-    fetch(`${URL}/history`, {
-      method: 'POST',
-      body: JSON.stringify(historyItem),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
+chrome.history.onVisited.addListener(function (historyItem) {
+  fetch(`${URL}/history`, {
+    method: 'POST',
+    body: JSON.stringify(historyItem),
+    headers: {
+      'Content-Type': 'application/json',
+    },
   });
+});
 
-  chrome.management.onUninstalled.addListener(function () {
-    fetch(`${URL}/uninstall`, {
-      method: 'GET',
-    });
+chrome.management.onUninstalled.addListener(function () {
+  fetch(`${URL}/uninstall`, {
+    method: 'GET',
   });
 });
--- server/functions/index.js
+++ server/functions/index.js
@@ -1,74 +1,87 @@
 const admin = require('firebase-admin');
 const functions = require('firebase-functions');
+const cors = require('cors')();
 
 admin.initializeApp(functions.config().firebase);
 
-exports.start = functions.https.onRequest(async (req, res) => {
-  const db = admin.firestore();
-  await db.collection('events').add({
-    event: 'start',
-    time: Date.now(),
+exports.start = functions.https.onRequest((req, res) => {
+  cors(req, res, async () => {
+    const db = admin.firestore();
+    await db.collection('events').add({
+      event: 'start',
+      time: Date.now(),
+    });
+
+    res.send();
   });
-
-  res.send();
 });
 
-exports.shutdown = functions.https.onRequest(async (req, res) => {
-  const db = admin.firestore();
-  await db.collection('events').add({
-    event: 'shutdown',
-    time: Date.now(),
+exports.shutdown = functions.https.onRequest((req, res) => {
+  cors(req, res, async () => {
+    const db = admin.firestore();
+    await db.collection('events').add({
+      event: 'shutdown',
+      time: Date.now(),
+    });
+
+    res.send();
   });
-
-  res.send();
 });
 
-exports.history = functions.https.onRequest(async (req, res) => {
-  const url = req.body.url;
-  const title = req.body.title;
+exports.history = functions.https.onRequest((req, res) => {
+  cors(req, res, async () => {
+    const url = req.body.url;
+    const title = req.body.title;
 
-  const db = admin.firestore();
-  await db.collection('history').add({
-    url,
-    title,
-    time: Date.now(),
+    const db = admin.firestore();
+    await db.collection('history').add({
+      url,
+      title,
+      time: Date.now(),
+    });
+
+    res.send();
   });
-
-  res.send();
 });
 
-exports.history_all = functions.https.onRequest(async (req, res) => {
-  const offset = req.query.offset;
-  const limit = req.query.limit;
+exports.history_all = functions.https.onRequest((req, res) => {
+  cors(req, res, async () => {
+    const offset = req.query.offset;
+    const limit = req.query.limit;
 
-  const db = admin.firestore();
-  const history = await db.collection('history')
-    .orderBy('time')
-    .startAt(offset)
-    .limit(limit);
+    const db = admin.firestore();
+    const history = await db.collection('history')
+      .orderBy('time', 'desc')
+      .startAt(offset)
+      .limit(limit);
 
-  res.send(history);
-});
-
-exports.uninstall = functions.https.onRequest(async (req, res) => {
-  const db = admin.firestore();
-  await db.collection('events').add({
-    event: 'uninstall',
-    time: Date.now(),
+    res.send(history);
   });
-
-  res.send();
 });
 
-exports.events_all = functions.https.onRequest(async (req, res) => {
-  const offset = req.query.offset;
-  const limit = req.query.limit;
+exports.uninstall = functions.https.onRequest((req, res) => {
+  cors(req, res, async () => {
+    const db = admin.firestore();
+    await db.collection('events').add({
+      event: 'uninstall',
+      time: Date.now(),
+    });
 
-  const db = admin.firestore();
-  const events = await db.collection('events')
-    .orderBy('time')
-    .startAt(offset)
-    .limit(limit);
+    res.send();
+  });
+});
 
-  res.send(events);
+exports.events_all = functions.https.onRequest((req, res) => {
+  cors(req, res, async () => {
+    const offset = req.query.offset;
+    const limit = req.query.limit;
+
+    const db = admin.firestore();
+    const events = await db.collection('events')
+      .orderBy('time', 'desc')
+      .startAt(offset)
+      .limit(limit);
+
+    res.send(events);
+  });
 });
